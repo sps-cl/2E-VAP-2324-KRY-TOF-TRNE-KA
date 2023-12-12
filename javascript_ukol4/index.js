027e8f930ece9286faa87e94443f6b8d1c7712ce
KrystofTrnecka <147551588+KrystofTrnecka@users.noreply.github.com>
2023-12-12 10:47:24 +0100

    Add files via upload

---
+++ javascript_ukol4/index.js
@@ -0,0 +1,17 @@
+let pupUp = new popupWindows();
+
+class Osoba{
+    constructor(jmeno,prijmeni,vek){
+      this.jmeno = jmeno
+      this.prijmeni = prijmeni
+      this.vek = vek
+    }
+    toString(){
+        return "jméno: "+ this.jmeno +", přijmení: "+ this.prijmeni
+    }
+}
+
+function osoba(){
+    
+    instance.const this.tlacitko = new type(arguments);
+}
